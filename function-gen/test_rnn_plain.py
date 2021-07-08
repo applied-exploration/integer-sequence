1eb0f0df716e7973248283f143bde0c3bcbb9e5e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2021-07-08 15:23:21 +0200

    fix(CI): use conda to install dependencies

--- function-gen/test_rnn_plain.py
+++ function-gen/test_rnn_plain.py
@@ -5,6 +5,7 @@ from lang import load_data_int_seq
 from utils import accuracy_score
 
 output_lang, input_lang, train, X_test, y_test = load_data_int_seq()
+train = train[:10]
 
 algo = RNN_Plain(symbols = "+*-0123456789t", 
 output_sequence_length = 9, 
@@ -13,13 +14,13 @@ num_epochs = 5000,
 input_size = input_lang.n_words, 
 hidden_size = 256, 
 output_size=output_lang.n_words, 
-embedding_size = 28, 
-batch_size = 32, 
+embedding_size = 256, 
+batch_size = 1, 
 calc_magnitude_on=False)
 
 algo.train(input_lang, output_lang, train)
 
-pred = algo.infer(input_lang, output_lang, X_test[:1000])
-pred[:25]
+# pred = algo.infer(input_lang, output_lang, X_test[:1000])
+# pred[:25]
 
-accuracy_score(pred, y_test[:1000])
+# accuracy_score(pred, y_test[:1000])
