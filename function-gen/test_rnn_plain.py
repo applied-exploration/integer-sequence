a8ecda9012af2146758c4138337be86457a9aace
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2021-07-09 11:18:30 +0200

    feat(CI): added super basic workflow (#21)
    * feat(CI): added super basic workflow
    
    * feat(CI): added environment.yaml
    
    * fix(CI): use conda to install dependencies
    
    * fix(CI): using the correct test file name
    
    * fix(CI): using correct yaml file name
    
    * feat(CI): added primite first test case
    
    * fix(Test): added new parameters
    
    * chore(Test): commented code not working inference code, to successfully run the test
    
    * fix(CI): added conda init
    
    * chore(CI): separate step to setup the env & train the model
    
    * fix(CI): use setup-miniconda step
    
    * fix(CI): use the correct yaml file name
    
    * chore(CI): added debug lines
    
    * chore(CI): separate activate step
    
    * chore(CI): added conda info and list to debug
    
    * chore(CI): added more debugging
    
    * fix(CI): use the correct shell when running conda
    
    * fix(CI): change folders in the current step
    
    * fix(CI): location of results.txt

---
+++ function-gen/test_rnn_plain.py
@@ -0,0 +1,36 @@
+from models.rnn.rnn_plain import RNN_Plain
+from models.rnn.rnn_attention import RNN_Attention
+
+from lang import load_data_int_seq
+from utils import accuracy_score
+
+output_lang, input_lang, train, X_test, y_test = load_data_int_seq()
+train = train[:10]
+
+algo = RNN_Plain(symbols = "+*-0123456789t", 
+output_sequence_length = 9, 
+encoded_seq_length = 9, 
+num_epochs = 1000, 
+input_size = input_lang.n_words, 
+hidden_size = 256, 
+output_size=output_lang.n_words, 
+embedding_size = 256, 
+batch_size = 1, 
+num_gru_layers = 1,
+dropout_prob = 0.,
+calc_magnitude_on=False)
+
+algo.train(input_lang, output_lang, train)
+
+# calculate accuracy from the training set (fails because of something after mini-batch is implemented)
+
+# pred = algo.infer(input_lang, output_lang, [i[0] for i in train])
+# pred[:10]
+# accuracy_score(pred, [i[0] for i in train])
+
+
+# calculate accuracy from the test set
+
+# pred = algo.infer(input_lang, output_lang, X_test[:1000])
+# pred[:25]
+# accuracy_score(pred, y_test[:1000])
