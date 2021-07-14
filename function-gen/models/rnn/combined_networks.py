32c35531a5ad71c198624e3824de1090252bc555
Daniel Szemerey <szemy2@gmail.com>
2021-07-14 14:44:07 +0200

    fix: random library is connected accross files and I was calling it in a place over and over again. (#38)

--- function-gen/models/rnn/combined_networks.py
+++ function-gen/models/rnn/combined_networks.py
@@ -13,7 +13,6 @@ MAX_LENGTH = 10
 
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, input_lang, output_lang, with_attention = False, calc_magnitude = None, max_length=MAX_LENGTH, seed = 1):
-    random.seed(seed)
 
     encoder_optimizer.zero_grad()
     decoder_optimizer.zero_grad()
