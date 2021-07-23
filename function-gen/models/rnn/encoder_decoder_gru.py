d15ebda8d078dd099d43e3a9dad307749ab4d084
Daniel Szemerey <szemy2@gmail.com>
2021-07-23 18:26:05 +0200

    feature: arbitary number of cnns in progress.

--- function-gen/models/rnn/encoder_decoder_gru.py
+++ function-gen/models/rnn/encoder_decoder_gru.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 
 import torch.optim as optim
 
+from typing import List, Tuple
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def dec2bin(x, bits):
@@ -19,7 +21,7 @@ BINARY_NUM = 32
 
 class EncoderRNN(nn.Module):
 
-    def __init__(self, input_size:int, hidden_size:int, embedding_size:int, batch_size:int, cnn_output_depth: int = 512, cnn_kernel_size:int = 3, num_gru_layers:int = 1, dropout:float = 0.0,  seed:int = 1, bidirectional:bool=False, binary_encoding:bool = False) -> None:
+    def __init__(self, input_size:int, hidden_size:int, embedding_size:int, batch_size:int, cnn_output_depth: List[int] = [512], cnn_kernel_size:int = 3, num_gru_layers:int = 1, dropout:float = 0.0,  seed:int = 1, bidirectional:bool=False, binary_encoding:bool = False) -> None:
 
         super(EncoderRNN, self).__init__()
         
@@ -27,7 +29,8 @@ class EncoderRNN(nn.Module):
 
         self.hidden_size = hidden_size
         self.batch_size = batch_size
-        self.num_layers = num_gru_layers
+        self.num_gru_layers = num_gru_layers
+        self.cnn_output_depth = cnn_output_depth
         self.bidirectional = bidirectional
 
         self.binary_encoding = binary_encoding
@@ -36,10 +39,18 @@ class EncoderRNN(nn.Module):
     
         if binary_encoding == True:
             embedding_size = BINARY_NUM
-          
-        self.cnn = nn.Conv1d(embedding_size, cnn_output_depth, kernel_size=cnn_kernel_size, stride=1, padding=1)
-        self.gru = nn.GRU(cnn_output_depth, hidden_size, num_layers=num_gru_layers, dropout = dropout, bidirectional=self.bidirectional)
+        
+        cnn_list = [nn.Conv1d(i, cnn_output_depth[i+1], kernel_size=cnn_kernel_size, stride=1, padding=1)
+                for i in range(len(cnn_output_depth))]
 
+        cnn_list.insert(0, nn.Conv1d(embedding_size, cnn_output_depth, kernel_size=cnn_kernel_size, stride=1, padding=1))
+
+        print(cnn_list)
+        self.cnn = nn.ModuleList(cnn_list)
+
+        self.batch_norm = nn.BatchNorm1d(cnn_output_depth)
+        self.gru = nn.GRU(cnn_output_depth, hidden_size, num_layers=num_gru_layers, dropout = dropout, bidirectional=self.bidirectional)
+        
 
 
     def forward(self, input, hidden):
@@ -78,7 +89,10 @@ class EncoderRNN(nn.Module):
         # print("embedded output", embedded.shape)
         embedded = embedded.transpose(0,1).transpose(1,2)
 
-        output =  self.cnn(embedded)
+        output = embedded
+        for i in range(len(self.cnn_output_depth)):
+            output = self.cnn[i](output)
+            # output = self.batch_norm(output)
 
         output = output.transpose(0,1).transpose(0,2)
         # print("CNN output reshaped", output.shape)
@@ -105,8 +119,8 @@ class EncoderRNN(nn.Module):
     def initHidden(self, batch_size = None):
         if batch_size == None : batch_size = self.batch_size
 
-        if self.bidirectional: return torch.zeros(2 * self.num_layers, batch_size, self.hidden_size, device=device)
-        else: return torch.zeros(self.num_layers, batch_size, self.hidden_size, device=device) 
+        if self.bidirectional: return torch.zeros(2 * self.num_gru_layers, batch_size, self.hidden_size, device=device)
+        else: return torch.zeros(self.num_gru_layers, batch_size, self.hidden_size, device=device) 
 
 
 class DecoderRNN(nn.Module):
--- function-gen/models/rnn/rnn_plain.py
+++ function-gen/models/rnn/rnn_plain.py
@@ -1,26 +1,21 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-
-from typing import List, Tuple
-from learning_types import LearningAlgorithm
-
-
-import math
-import time
-import random
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-
-from .encoder_decoder_gru import EncoderRNN, DecoderRNN
-from .combined_networks import train, infer, Loss
-from .rnn_utils import tensorFromSentence, calc_magnitude
-from utils import timeSince
-from lang import Lang
-
 import wandb
+from lang import Lang
+from utils import timeSince
+from .rnn_utils import tensorFromSentence, calc_magnitude
+from .combined_networks import train, infer, Loss
+from .encoder_decoder_gru import EncoderRNN, DecoderRNN
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import random
+import time
+import math
+from learning_types import LearningAlgorithm
+from typing import List, Tuple
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 
 # from line_profiler import LineProfiler
@@ -32,11 +27,30 @@ SOS_token = 1
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 class RNN_Plain(LearningAlgorithm):
 
+    def __init__(self,
+                 symbols: List[str],
+                 output_sequence_length: int,
+                 encoded_seq_length: int,
+                 num_epochs: int,
+                 input_size: int,
+                 output_size: int,
+                 hidden_size: int = 256,
+                 embedding_size: int = 64,
+                 batch_size: int = 2,
+                 learning_rate: float = 0.01,
+                 num_gru_layers: int = 1,
+                 dropout_prob: float = 0.0,
+                 loss: Loss = Loss.NLL,
+                 cnn_output_depth: List[int] = [512],
+                 cnn_kernel_size: int = 3,
+                 seed: int = 1,
+                 bidirectional: bool = False,
+                 wandb_activate: bool = True,
+                 binary_encoding: bool = False):
 
-    def __init__(self, symbols: List[str], output_sequence_length: int, encoded_seq_length: int,  num_epochs: int, input_size:int, output_size:int, hidden_size: int = 256, embedding_size:int = 64, batch_size:int = 2, learning_rate: float = 0.01, num_gru_layers: int = 1, dropout_prob: float = 0.0, loss:Loss = Loss.NLL, cnn_output_depth:int=512, cnn_kernel_size:int=3, seed:int = 1, bidirectional:bool=False, wandb_activate:bool=True, binary_encoding:bool = False):
-        
         random.seed(seed)
         self.wandb_activate = wandb_activate
 
@@ -54,19 +68,20 @@ class RNN_Plain(LearningAlgorithm):
         self.embedding_size = embedding_size
         self.batch_size = batch_size
 
-        self.encoder = EncoderRNN(self.input_size, self.hidden_size, self.embedding_size, self.batch_size, cnn_output_depth = cnn_output_depth, cnn_kernel_size = cnn_kernel_size, num_gru_layers=num_gru_layers, dropout=dropout_prob, seed=seed, bidirectional=bidirectional, binary_encoding= binary_encoding).to(device)
-        self.decoder = DecoderRNN(self.hidden_size, self.output_size, self.embedding_size, self.batch_size, num_gru_layers=num_gru_layers, dropout=dropout_prob, seed=seed, bidirectional_encoder=bidirectional).to(device)
+        self.encoder = EncoderRNN(self.input_size, self.hidden_size, self.embedding_size, self.batch_size, cnn_output_depth=cnn_output_depth,
+                                  cnn_kernel_size=cnn_kernel_size, num_gru_layers=num_gru_layers, dropout=dropout_prob, seed=seed, bidirectional=bidirectional, binary_encoding=binary_encoding).to(device)
+        self.decoder = DecoderRNN(self.hidden_size, self.output_size, self.embedding_size, self.batch_size,
+                                  num_gru_layers=num_gru_layers, dropout=dropout_prob, seed=seed, bidirectional_encoder=bidirectional).to(device)
 
-        
-        if self.wandb_activate: 
+        if self.wandb_activate:
             wandb.watch(self.encoder, log_freq=100)
             wandb.watch(self.decoder, log_freq=100)
-        
+
         print("Num_batch: ", self.batch_size)
         print(self.encoder)
         print(self.decoder)
 
-    # WITH DATALOADER =>   
+    # WITH DATALOADER =>
     # def convert_data(self, data:List[Tuple[List[int], str]]) -> List[Tuple[str, str]]:
     #     converted_data = []
 
@@ -81,7 +96,7 @@ class RNN_Plain(LearningAlgorithm):
     #     return [tensorFromSentence(lang, data[i])
     #                     for i in range(len(data))]
 
-    def seperate_data(self, data:List[Tuple[List[int], str]]) -> Tuple[List[str], List[str]]:
+    def seperate_data(self, data: List[Tuple[List[int], str]]) -> Tuple[List[str], List[str]]:
         ''' 
         Description: 
             Function that seperates input and target into seperate lists
@@ -105,13 +120,12 @@ class RNN_Plain(LearningAlgorithm):
 
         return input_data, target_data
 
-    def create_minibatch(self, data: List[str], lang: Lang, indices:List[int]) -> torch.tensor:
-        encoded_dataset = [tensorFromSentence(lang, data[index]) for index in indices]
-        
-        ## flatten it to a batch tensor, one_column = one batch of sequence, one_row = time step in sequences
-        return torch.cat(encoded_dataset, dim=1) 
+    def create_minibatch(self, data: List[str], lang: Lang, indices: List[int]) -> torch.tensor:
+        encoded_dataset = [tensorFromSentence(
+            lang, data[index]) for index in indices]
 
-
+        # flatten it to a batch tensor, one_column = one batch of sequence, one_row = time step in sequences
+        return torch.cat(encoded_dataset, dim=1)
 
     def train(self, input_lang: Lang, output_lang: Lang, data: List[Tuple[List[int], str]]) -> None:
         print_every = max(1, math.floor(self.num_epochs/10))
@@ -120,10 +134,11 @@ class RNN_Plain(LearningAlgorithm):
         start = time.time()
         print_loss_total = 0  # Reset every print_every
 
-
         ''' Defining Optimization parameters'''
-        encoder_optimizer = optim.SGD(self.encoder.parameters(), lr=self.learning_rate)
-        decoder_optimizer = optim.SGD(self.decoder.parameters(), lr=self.learning_rate)
+        encoder_optimizer = optim.SGD(
+            self.encoder.parameters(), lr=self.learning_rate)
+        decoder_optimizer = optim.SGD(
+            self.decoder.parameters(), lr=self.learning_rate)
 
         ''' 
         NLLLos requires 
@@ -136,38 +151,38 @@ class RNN_Plain(LearningAlgorithm):
               shaped [num_batches]. 
               eg.: [2, 5, 15] with num_batch 3. Each number represents a category
         '''
-        criterion = nn.NLLLoss()  
+        criterion = nn.NLLLoss()
 
         ''' Prepare Data '''
         input_data, target_data = self.seperate_data(data)
 
-        # --- with DataLoader --- # 
+        # --- with DataLoader --- #
         # train_dataloader = DataLoader((self.dataset_to_tensor(input_data, input_lang),self.dataset_to_tensor(target_data, output_lang)),
         #      batch_size=self.batch_size, shuffle=True)
         # input_tensor, target_tensor = next(iter(train_dataloader))
 
-        
-
         ''' Feed forward of network & calculating loss'''
         for i in range(1,  self.num_epochs + 1):
-            
+
             ''' Create a minibatch tensor [sequence_len, batch_size]'''
             # --- with own minibatching --- #
-            randomized_indices = [random.randrange(0, len(data)) for _ in range(0, self.batch_size)]
-            input_tensor_minibatch = self.create_minibatch(input_data, input_lang, randomized_indices)
-            target_tensor_minibatch = self.create_minibatch(target_data, output_lang, randomized_indices)
- 
+            randomized_indices = [random.randrange(
+                0, len(data)) for _ in range(0, self.batch_size)]
+            input_tensor_minibatch = self.create_minibatch(
+                input_data, input_lang, randomized_indices)
+            target_tensor_minibatch = self.create_minibatch(
+                target_data, output_lang, randomized_indices)
+
             # --- with DataLoader --- #
             # input_tensor, target_tensor = next(iter(train_dataloader))
-                  
 
             loss = train(
-                input_tensor_minibatch, target_tensor_minibatch, 
-                self.encoder, self.decoder, 
-                encoder_optimizer, decoder_optimizer, 
-                criterion, 
-                input_lang, output_lang, 
-                loss_type = self.loss )
+                input_tensor_minibatch, target_tensor_minibatch,
+                self.encoder, self.decoder,
+                encoder_optimizer, decoder_optimizer,
+                criterion,
+                input_lang, output_lang,
+                loss_type=self.loss)
 
             print_loss_total += loss
 
@@ -176,23 +191,24 @@ class RNN_Plain(LearningAlgorithm):
                 print_loss_avg = print_loss_total / print_every
                 print_loss_total = 0
 
-                if self.wandb_activate: wandb.log({'loss': print_loss_avg, 'epoch': i})
+                if self.wandb_activate:
+                    wandb.log({'loss': print_loss_avg, 'epoch': i})
                 print('%s (%d %d%%) %.4f' % (timeSince(start, i / self.num_epochs),
-                                            i, i / self.num_epochs * 100, print_loss_avg))
-
-
+                                             i, i / self.num_epochs * 100, print_loss_avg))
 
     def infer(self, input_lang: Lang, output_lang: Lang, data: List[List[int]]) -> List[str]:
         ''' Prepare data '''
-        stringified_inputs = [''.join(str(x)+',' for x in sequence) for sequence in data]
-        
-        input_tensor_batch = self.create_minibatch(stringified_inputs, input_lang, list(range(0, len(data))))
-        output_list = infer(input_tensor_batch, self.encoder, self.decoder, output_lang )
+        stringified_inputs = [
+            ''.join(str(x)+',' for x in sequence) for sequence in data]
+
+        input_tensor_batch = self.create_minibatch(
+            stringified_inputs, input_lang, list(range(0, len(data))))
+        output_list = infer(input_tensor_batch, self.encoder,
+                            self.decoder, output_lang)
 
         return output_list
-        
+
     def save(self, name: str):
         folder = ""
         torch.save(self.encoder.state_dict(), name + "-encoder.pt")
         torch.save(self.decoder.state_dict(), name + "-decoder.pt")
-
