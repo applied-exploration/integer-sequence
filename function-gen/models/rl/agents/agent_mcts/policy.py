143452a70ce469853dc2d74d416faa283ab42b2e
Daniel Szemerey <szemy2@gmail.com>
2021-08-03 14:15:08 +0200

    feature: MCTS base implementation (#64)
    * feature: Added base code from sample implementation.
    
    * feature: Applying own environment.
    
    * feature: Modified the environment to both contain flattened and unflattened states.
    
    * feature: Systems runs until adding observations to replaybuffer.
    
    * feat: Some changes in print file and concationation.

---
+++ function-gen/models/rl/agents/agent_mcts/policy.py
@@ -0,0 +1,61 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class HillClimbingPolicy(nn.Module):
+    """
+    Simple neural network policy for solving the hill climbing task.
+    Consists of one common dense layer for both policy and value estimate and
+    another dense layer for each.
+    """
+
+    def __init__(self, n_obs, n_hidden, n_actions):
+        super(HillClimbingPolicy, self).__init__()
+
+        self.n_obs = n_obs
+        self.n_hidden = n_hidden
+        self.n_actions = n_actions
+
+        self.dense1 = nn.Linear(n_obs, n_hidden)
+        self.dense_p = nn.Linear(n_hidden, n_actions)
+        self.dense_v = nn.Linear(n_hidden, 1)
+
+    def forward(self, obs):
+        # print("policy forward")
+        # print(obs.shape[0])
+        # print(self.n_obs)
+        # print("obs")
+        # print(obs)
+
+        # obs_one_hot = torch.zeros((obs.shape[0], self.n_obs))
+        # print("obs_one_hot")
+        # print(obs_one_hot)
+        # obs_one_hot[np.arange(obs.shape[0]), obs.numpy()] = 1.0
+        # print(obs_one_hot)
+        # h_relu = F.relu(self.dense1(obs_one_hot))
+
+        # print(obs.shape)
+        h_relu = F.relu(self.dense1(obs))
+        # print(h_relu.shape)
+        logits = self.dense_p(h_relu)
+        # print(logits.shape)
+        policy = F.softmax(logits, dim=1)
+        # print(policy.shape)
+
+        value = self.dense_v(h_relu).view(-1)
+
+        return logits, policy, value
+
+    def step(self, obs):
+        """
+        Returns policy and value estimates for given observations.
+        :param obs: Array of shape [N] containing N observations.
+        :return: Policy estimate [N, n_actions] and value estimate [N] for
+        the given observations.
+        """
+        obs = torch.from_numpy(obs)
+        _, pi, v = self.forward(obs)
+
+        return pi.detach().numpy(), v.detach().numpy()
--- function-gen/models/rl/env.py
+++ function-gen/models/rl/env.py
@@ -7,8 +7,11 @@ import os
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from lang import Lang
-from utils import flatten
+from utils import flatten, eq_to_seq, is_eq_valid, normalize_0_1
+
 import random
+import numpy as np
+
 
 MAX_PENALTY_MAGNITUDE = 999.0
 
@@ -60,7 +63,10 @@ def decode_with_lang(lang: Lang, input):
 def create_initial_state(input_lang: Lang, data: List[List[int]], output_length: int) -> TreeState:
     int_seq = random.choice(data)[0]
     seq = encode_with_lang(input_lang, [str(i) for i in int_seq])
-    return ([-1] * output_length, seq)
+
+    unflattened = ([-1] * output_length, seq)
+    flattened = flatten(([-1] * output_length, seq))
+    return flattened, unflattened
 
 
 def get_current_position(state: TreeState) -> int:
@@ -110,34 +116,111 @@ class IntegerSequenceEnv(gym.Env):
         self.output_length = env_config["output_length"]
         self.input_lang = env_config["input_lang"]
         self.output_lang = env_config["output_lang"]
-        self.evaluate = env_config["evaluate"]
+        self.evaluate = evaluate_candidate_eq#env_config["evaluate"]
         self.data = env_config["data"]
-        self.state = create_initial_state(self.input_lang, self.data, self.output_length)
+        self.state, self.unflattened_state = create_initial_state(self.input_lang, self.data, self.output_length)
 
+        print("self.state")
+        print(self.state)
         self.action_space = spaces.Discrete(len(self.syms))
         # if we ever want to return to tuple state
         # self.observation_space = spaces.Tuple((spaces.Box(low=-1, high=self.output_lang.n_words, shape=(self.output_length,), dtype= int), spaces.Box(low=0, high=self.input_lang.n_words, shape=(len(self.state[1]),), dtype= int)))
-        self.observation_space = spaces.Box(low=-1, high=self.output_lang.n_words, shape=(self.output_length + len(self.state[1]),), dtype= int)
+        self.observation_space = spaces.Box(low=-1, high=self.output_lang.n_words, shape=(len(self.state),), dtype= int)
 
 
     def step(self, action):
         # offset action by 2, because we don't need SOS and EOS tokens here
         action = action + 2
-        if not is_action_valid(self.state, action, self.output_lang):
-            return (insert_action_in_state((self.state[0].copy(), self.state[1]), action), -MAX_PENALTY_MAGNITUDE, True, {})
-
-        self.state = insert_action_in_state((self.state[0].copy(), self.state[1]), action)
-        if is_state_complete(self.state):
-            candidate_eq = ''.join(decode_with_lang(self.output_lang, self.state[0]))
-            score = self.evaluate(candidate_eq, self.state[1])
-            return (flatten(self.state), score, True, {})
         
-        return (flatten(self.state), 0, False, {})
+        self.unflattened_state = insert_action_in_state((self.unflattened_state[0].copy(), self.unflattened_state[1]), action)
+        self.state = flatten(self.unflattened_state)
+
+        if not is_action_valid(self.unflattened_state, action, self.output_lang):
+            return (self.state, -MAX_PENALTY_MAGNITUDE, True, {})
+
+        if is_state_complete(self.unflattened_state):
+            candidate_eq = ''.join(decode_with_lang(self.output_lang, self.unflattened_state[0]))
+            score = self.evaluate(candidate_eq, self.unflattened_state[1])
+            return (self.state, score, True, {})
+        
+        return (self.state, 0, False, {})
  
 
+    # def reset(self):
+    #     self.state = create_initial_state(self.input_lang, self.data, self.output_length)
+    #     return flatten(self.state)
     def reset(self):
-        self.state = create_initial_state(self.input_lang, self.data, self.output_length)
-        return flatten(self.state)
+        self.state, self.unflattened_state = create_initial_state(self.input_lang, self.data, self.output_length)
+        return (self.unflattened_state, 0, False, {})
 
     def render(self, mode='human', close=False):
         return print(self.state)
+
+    @staticmethod
+    def get_obs_for_states(states):
+        # print(states)
+        # print(np.array(*states))
+        return np.array(states)
+
+    # @staticmethod
+    def next_state(self, state, action, shape=(7, 7)):
+        action = action + 2
+        if not is_action_valid(state, action, self.output_lang):
+            return (insert_action_in_state((state[0].copy(), state[1]), action), -MAX_PENALTY_MAGNITUDE, True, {})
+
+        state = insert_action_in_state((state[0].copy(), state[1]), action)
+        if is_state_complete(state):
+            candidate_eq = ''.join(decode_with_lang(self.output_lang, state[0]))
+            score = self.evaluate(candidate_eq, state[1])
+            return (state, score, True, {})
+        
+        return (state, 0, False, {})
+        # return pos[0] * shape[0] + pos[1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def evaluate_candidate_eq(candidate: str, int_seq: List[int]) -> float:
+    if is_eq_valid(candidate) == False:
+        return -MAX_PENALTY_MAGNITUDE
+
+    output_sequence = eq_to_seq(candidate, 9)
+
+    if np.count_nonzero(output_sequence) < 1:
+        return -MAX_PENALTY_MAGNITUDE
+
+    return compare_sequences(output_sequence, int_seq)
+
+def compare_sequences(output_sequence: List[int], target_sequence: List[int]) -> float:
+
+    if len(output_sequence) != len(target_sequence):
+        raise AssertionError("sequence size don't match: " + ','.join(str(e) for e in output_sequence)
+ + " | " + ','.join(str(e) for e in target_sequence))
+
+    magnitude: float = 0.0
+
+    combined_seq = np.vstack([output_sequence, target_sequence]) 
+    norm_comb_seq = normalize_0_1(combined_seq)
+
+    norm_output_seq = norm_comb_seq[0]
+    norm_target_seq = norm_comb_seq[1]
+
+    
+    for x, y in zip(norm_target_seq, norm_output_seq):
+        magnitude += abs(x - y)#**2
+
+    # magnitude /= len(norm_target_seq)
+
+    return 10 - (magnitude * 100)
+
